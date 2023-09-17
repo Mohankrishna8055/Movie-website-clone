954a8cd2d667e9245284f734ec4cdf9ab3cf656a
Mohankrishna8055 <mohankrishnabommali@gmail.com>
2023-09-17 15:55:20 +0530

    added youtube links to each poster according to the poster

--- Netflix clone/netflix.js
+++ Netflix clone/netflix.js
@@ -5,63 +5,63 @@ let movies =[
         RealeaseYear : "2020",
         Rating :7.1,
         poster:"https://www.slashfilm.com/wp/wp-content/images/2017-bestposter-split-700x1082.jpg",
-        src:"https://youtu.be/84TouqfIsiI?si=5MErmh7LRO6i9Ksg"
+        src:"https://www.youtube-nocookie.com/embed/84TouqfIsiI?si=dqcLKQjoc2D5x0u8"
     },
     {
         MovieName: "Cars 3",
         RealeaseYear : "2006",
         Rating :8.5,
         poster:"https://www.slashfilm.com/wp/wp-content/images/2017-bestposter-cars3-700x1037.jpg",
-        src:"https://youtu.be/2LeOH9AGJQM?si=tOZhERtN3mCp2iFw"
+        src:"https://www.youtube-nocookie.com/embed/ZuaseSovWDY?si=75_Y6Xb0oVGUV08G"
     },
     {
         MovieName: "Justice Leauge",
         RealeaseYear : "2005",
         Rating :7.4,
         poster:"https://www.slashfilm.com/wp/wp-content/images/2017-bestposter-justiceleague-700x1038.jpg",
-        src:"https://youtu.be/3cxixDgHUYw?si=exHfZoo5CCLZ9mcL"
+        src:"https://www.youtube-nocookie.com/embed/oTXrl8H6luI?si=mSAaUaKHxOtYwbFW"
     },
     {
         MovieName: "Blade Runner",
         RealeaseYear : "2012",
         Rating :6.1,
         poster:"https://www.slashfilm.com/wp/wp-content/images/2017-bestposter-bladerunner2049-700x1038.jpg",
-        src:"https://youtu.be/gCcx85zbxz4?si=0IPS7HzyUXcSqb90"
+        src:"https://www.youtube-nocookie.com/embed/dZOaI_Fn5o4?si=y9BC4zqG9bt0qMRZ"
     },
     {
         MovieName: "Star Wars",
         RealeaseYear : "2016",
         Rating :7.1,
         poster:"https://www.slashfilm.com/wp/wp-content/images/2017-bestposter-starwarslastjedi-700x1038.jpg",
-        src:"https://youtu.be/8Qn_spdM5Zg?si=zLOUdP5et46dCuFF"
+        src:"https://www.youtube-nocookie.com/embed/sGbxmsDFVnE?si=8ZD80cDH1XZT9Yij"
     },
     {
         MovieName: "Thor ",
         RealeaseYear : "2021",
         Rating :9.1,
         poster:"https://www.slashfilm.com/wp/wp-content/images/2017-bestposter-thorragnarok-700x1038.jpg",
-        src:"https://youtu.be/JOddp-nlNvQ?si=B_9Cey8pNii6olSF"
+        src:"https://www.youtube-nocookie.com/embed/JOddp-nlNvQ?si=i2OeNyKKC17NnHrO"
     },
     {
         MovieName: "Zombie reddy",
         RealeaseYear : "2021",
         Rating :8.1,
         poster:"https://i0.wp.com/chitrambhalare.in/wp-content/uploads/2020/10/Zombie-Reddy-telugu-movie-first-look-poster.jpg?w=723&ssl=1",
-        src:"https://youtu.be/qCUrcgcXDtE?si=-ltm_oiVkSIgk9ky"
+        src:"https://www.youtube-nocookie.com/embed/qCUrcgcXDtE?si=O-20rFC_ydPsJE5q"
     },
     {
         MovieName: "Sahooo",
         RealeaseYear : "2021",
         Rating :10,
         poster:"https://2.bp.blogspot.com/-wUjYQryFhgs/We4gy11q4zI/AAAAAAAAqOU/x2_WvejTNGYG8kkU6TE_VNCzNU0jkQdOQCLcBGAs/s320/saaho-first.jpg",
-        src:"https://youtu.be/lD0-ztCFydA?si=2YJNszogikKq34bY"
+        src:"https://www.youtube-nocookie.com/embed/Jln4eyvzMNg?si=9yMfzPkFIQavmi9B"
     },
     {
         MovieName: "Mirchi ",
         RealeaseYear : "2021",
         Rating :9.7,
         poster:"https://wallpapercave.com/dwp1x/wp6685147.jpg",
-        src:"https://www.youtube-nocookie.com/embed/I5cJMydo5Hc?si=a8RgTGY4xBFT72-_"
+        src:"https://www.youtube-nocookie.com/embed/I6QLMyeC1n8?si=GJf1I9Ce9Q1Jtjtc"
     },
     {
         MovieName: "Leo ",
@@ -75,7 +75,7 @@ let movies =[
         RealeaseYear : "2025",
         Rating :9.1,
         poster:"https://cdn.123telugu.com/content/wp-content/uploads/2022/10/ProjectK.jpg",
-        src: "https://www.youtube.com/watch?si=kuGj1EYgdJIoGEEx&v=bC36d8e3bb0&feature=youtu.be"
+        src: "https://www.youtube-nocookie.com/embed/mkHMQ4RGqGg?si=n-eZU-m0nLbRTmip"
     }
     
 ];
@@ -84,19 +84,11 @@ let movies =[
 
 
 
-let movieSearchText = document.getElementById("movieSearch").value ;
-function searchMovie() {
-    console.log(movieSearchText);
-}
-
-
-
-
-function Moviesdisplay() {
+function Moviesdisplay(data) {
     
-    for (let i = 0; i < movies.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         let moviesSec = document.getElementById("movies");
-        let movie = movies[i];
+        let movie = data[i];
         let maindiv = document.createElement("div");
         maindiv.classList.add("maindiv")
 
@@ -117,13 +109,147 @@ function Moviesdisplay() {
                 </div>
             </div>
         `;
-        // console.log(maindiv.innerHTML);
         moviesSec.appendChild(maindiv);
     }
 }
 
 
-Moviesdisplay();
+Moviesdisplay(movies);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -174,5 +300,3 @@ Moviesdisplay();
 // }
 
 // Moviesdisplay();
-
-
