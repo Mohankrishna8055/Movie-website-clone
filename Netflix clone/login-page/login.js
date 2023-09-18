a01c1943592d5003db359449c57dd553fccb9f69
Bommali Mohankrishna <mohankrishnabommali@gmail.com>
2023-09-18 20:44:38 +0530

    added details for css js but failed

---
+++ Netflix clone/login-page/login.js
@@ -0,0 +1,15 @@
+const Uname = document.getElementById("Uname").value;
+const Uemail = document.getElementById("userEmailid").value;
+const Upassword = document.getElementById("userpassword").value;
+const verification = document.getElementById("verification").value;
+
+
+console.log(Uname);
+// function check(){
+//     if(Uname==""){
+//         window.alert("Enter the username to continue");
+//     }
+// }
+
+
+
